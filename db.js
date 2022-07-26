8087d744186d53cbfb1de1640521c280539ac959
ameedDevOps <106007570+ameedDevOps@users.noreply.github.com>
2022-07-26 18:32:49 +0530

    Customize DB url.

--- db.js
+++ db.js
@@ -15,7 +15,8 @@ const options = {
   connectTimeoutMS: 10000,
 };
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+//const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 mongoose.connect(url, options).then( function() {
   console.log('MongoDB is connected');
